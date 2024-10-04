8d288cfd631c72168e1cf12ac88875ace482adc0
Felipe Barcellos <felipe.cellos@hotmail.com>
2024-10-04 09:56:31 -0300

    uploaded lesson 5

--- respostas/cinco.py
+++ respostas/cinco.py
@@ -4,20 +4,23 @@ from perguntas import Pergunta
 class ExercicioCinco():
     def __init__(self, string) -> None:
         self.string: str = string
-        self.string_invertida: str
+        self.string_invertida = self.inverte_string(self.string)
         pass
 
     #Carregando os dados que serão consumidos
-    def inverte_string(self):
-        ...
+    def inverte_string(self, string):
+        invertida = ""
+        for i in range(len(string) -1, -1, -1):
+            invertida += string[i]
+        return invertida
         
     #Definindo representação do objeto
     def __repr__(self) -> str:
-        questao = Pergunta().retorna_pergunta(4)
+        questao = Pergunta().retorna_pergunta(5)
         string = ("-"*20)+"\n"
         string += questao+"\n"
-        string += f"\String: {self.string}:\n"
-        string += f"\String Invertida: {self.string_invertida}:\n"
+        string += f"String: {self.string}:\n"
+        string += f"String Invertida: {self.string_invertida}:\n"
         return string
     
 if __name__ == "__main__":
